214970f9b7e8f3d6f0e346e39292434531149f94
Nathan <101812777+corndogit@users.noreply.github.com>
2024-05-17 19:45:17 +0100

    Update compose to use GitHub container registry

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ A repository within which to clone Hard Brain's services and deploy with Docker 
 # Usage  
 
 1. Clone this repo
-2. Run the `get_repos.sh` script
-3. Set up the .env file as appropriate within each repo
-4. Run `docker compose up -d`
+2. Run initial_setup.sh to create required .env files
+3. For production, run `docker compose up -f compose-prod.yml -d`. For development, just run `docker compose up`.
--- initial_setup.sh
+++ initial_setup.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
 initial_setup() {
-	echo "Cloning reposititories..."
-
-    git clone git@hard-brain-api:hard-brain/hard-brain-api.git
-    git clone git@hard-brain-bot:hard-brain/hard-brain-bot.git
-
-    echo "Creating empty discord_token.env and postgres_password.env - please edit as appropriate"
-    echo "DISCORD_TOKEN=" >> discord_token.env
-    echo "POSTGRES_PASSWORD=" >> postgres_password.env
-
-    echo "Downloading songs..."
-    NEXTCLOUD_URL='https://corndog.galaxy.usbx.me/nextcloud/public.php/webdav/'
-    NEXTCLOUD_SHARE_ID='Q56JdfDpy8GMocd'
-    OUTPUT_ZIP='songs.zip'
-
-    read -p 'Enter download link password: ' -s nextcloud_download_pw
-    curl -u "$NEXTCLOUD_SHARE_ID:$nextcloud_download_pw" -H "X-Requested-With: XMLHttpRequest" $NEXTCLOUD_URL -o $OUTPUT_ZIP
-    if 7z x -o./hard-brain-api/src/resources/songs $OUTPUT_ZIP; then
-        echo "Successfully downloaded and extracted songs to ./hard-brain-api/src/resources/songs"
-    else
-        echo "Error: was unable to download/extract songs"
-    fi
+    read -p 'Enter Discord API token: ' -s discord_token
+    echo "DISCORD_TOKEN=${discord_token}" >> discord_token.env
+    echo "POSTGRES_PASSWORD=<your password here>" >> postgres_password.env
 }
 
 initial_setup
